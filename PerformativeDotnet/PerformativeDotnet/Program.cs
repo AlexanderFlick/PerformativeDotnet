3909b62585dd2ab2b5fc0c633ebb902e22617041
AlexanderFlick <macaronisupermoon@gmail.com>
2020-10-04 17:04:25 -0500

    first commit

---
+++ PerformativeDotnet/PerformativeDotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformativeDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
