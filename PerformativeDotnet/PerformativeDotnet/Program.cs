8a89cd1e300c4ea49f268b6801c6b00da7326219
AlexanderFlick <macaronisupermoon@gmail.com>
2020-10-04 17:14:10 -0500

    added first benchmark

--- PerformativeDotnet/PerformativeDotnet/Program.cs
+++ PerformativeDotnet/PerformativeDotnet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 
 namespace PerformativeDotnet
 {
@@ -6,7 +7,7 @@ namespace PerformativeDotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<DateParserBenchmark>();
         }
     }
 }
