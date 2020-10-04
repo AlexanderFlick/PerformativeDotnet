2ec4682ccdc2186331e5401ed135e73ff810e913
AlexanderFlick <macaronisupermoon@gmail.com>
2020-10-04 17:15:13 -0500

    added first benchmark

---
+++ PerformativeDotnet/PerformativeDotnet/DateParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PerformativeDotnet
+{
+    public class DateParser
+    {
+        public int GetYearFromDateTime(string dateTimeAsString)
+        {
+            var dateTime = DateTime.Parse(dateTimeAsString);
+            return dateTime.Year;
+        }
+    }
+}
---
+++ PerformativeDotnet/PerformativeDotnet/DateParserBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace PerformativeDotnet
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class DateParserBenchmark
+    {
+        private const string DateTime = "2020-10-10T12:00:00Z";
+        private static readonly DateParser Parser = new DateParser();
+
+        [Benchmark]
+        public void GetYearFromDateTime()
+        {
+            Parser.GetYearFromDateTime(DateTime);
+        }
+    }
+}
